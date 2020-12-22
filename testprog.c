50ee8794eb7f4862c085cd8fc671a489bef20145
Nomiram <qwerty.mironenko@gmail.com>
2020-12-22 03:41:10 +0300

    add comments

--- testprog.c
+++ testprog.c
@@ -7,16 +7,17 @@ int main()
     pid_t var1;
     printf("hello mai\n");
     var1 = fork();
-    if(var1 == 0)
+    if(var1 == 0)//if child
     {
     printf("next gen\n");
+	//execl((char*)program,(char*)program,(char*)arg1,(char*)arg2,...,NULL);
     execl("/bin/ps", "/bin/ps", NULL);
     }
-    else
+    else//if parent
     {
     printf("parent\n");
     }
-    printf("hello world\n");
+    printf("hello world\n");/*will not be shown to the child, because execl does not return control to this program*/
     wait(0);
     return 0;
 }
